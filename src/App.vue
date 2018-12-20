12c2b5a6a4bdd27a8309d6cd7a15171ba6eb91ba
Elvis Chan <elvis@thejibe.com>
2018-12-20 03:57:48 -0800

    Set up base style and re-organize structure

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,11 @@
       <button v-if="loggedout === null">Login</button>
       <button v-else @click="logout">Logout</button>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <script>
-import firebase from "firebase";
 import { mapActions } from "vuex";
 
 export default {
@@ -31,12 +30,43 @@ export default {
 };
 </script>
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700");
+
+html {
+  font-family: "Source Sans Pro", sans-serif;
+  font-size: 62.5%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-rendering: optimizeLegibility;
+  font-weight: 300;
+  line-height: $line-height;
+}
+
+body {
+  box-sizing: border-box;
+  font-size: 1.6rem;
+  background: $lighter-grey;
+}
+
+img {
+  max-width: 100%;
+}
+
+a {
+  text-decoration: none;
+
+  &:active,
+  &:hover,
+  &:visited {
+    outline-width: 0;
+    color: inherit;
+  }
+}
+ul,
+ol {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
 #nav {
   padding: 30px;
