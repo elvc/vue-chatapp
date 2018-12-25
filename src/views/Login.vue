eaa558f9624aac9cdb4cc31ad2b6787f0f958d96
Elvis Chan <elvis@thejibe.com>
2018-12-25 02:26:46 -0800

    Style login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="text-align: center;">
     <h3>Please login with your Google account to continue</h3>
     <button @click="login">Login with Google</button>
   </div>
